0c98684bb56c9dd108c61d3627285db7ca09a0d4
Sureshkumar04 <112899987+Sureshkumar04@users.noreply.github.com>
2024-04-24 10:50:01 +0530

    changes

---
+++ src/Compontents/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import '../Navbar/Navbar.css'
+import logo from 'F:/React Project/car_rental/src/assests/Logo.png'
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg mt-3 fixed-top">
+            <div className="container">
+                <img src={logo} alt="" />
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto align-items-center mb-2 mb-lg-0">
+                        <li className="nav-item me-3">
+                            <button className="nav-link active" aria-current="page" >Become a renter</button>
+                        </li>
+                        <li className="nav-item me-3">
+                            <button className="nav-link active" aria-current="page" >Rental deals</button>
+                        </li>
+                        <li className="nav-item me-3">
+                            <button className="nav-link active" aria-current="page" >How it work</button>
+                        </li>
+                        <li className="nav-item me-3">
+                            <button className="nav-link active" aria-current="page" >Why choose us</button>
+                        </li>
+                        
+                    </ul>
+                    <div className='text-center'>
+                        <button className='btn'>Sign In</button>
+                        <button className='btn btn-primary'>Sign up</button>
+                    </div>
+
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/Compontents/hero/Hero.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import '../hero/Hero.css'
+import Google from '../../assests/home/google play.png'
+import carback from '../../assests/home/Home left.png'
+import Car from '../../assests/home/car 2 1.png'
+import mark from '../../assests/home/Vector 1.png'
+
+export const Hero = () => {
+    return (
+        <>
+            <section className='full-height my-5 pt-5 my-md-0 pt-md-0 position-relative'>
+                <div className="row row-cols-1 row-cols-md-2 full-height g-0 align-items-center justify-content-center">
+                    <div className='col align-self-center'>
+                        <h1 className='mb-3 display-5 display-md-4 fw-semibold'>Find,book and rent a car <span className='position-relative'>Easily <img src={mark} className='position-absolute  top-50 pt-3 end-0 img-fluid' style={{ width: '100%' }} alt="" /></span></h1>
+                        <p className='mb-4'>Get a car wherever and whenever you need it with your IOS and Android device.</p>
+                        <img className='mb-4' src={Google} alt="" />
+                    </div>
+                    <div className='col align-self-end position-relative '>
+                        <img src={carback} className=' img-fluid' alt="" />
+                        <img src={Car} className=' img-fluid position-absolute bottom-0 mb-5 pb-5 mb-md-5 pb-md-0 end-0' alt="" />
+                    </div>
+                </div>
+                <div className="location justify-content-between row position-absolute bottom-0 mb-5 py-2 rounded d-none d-lg-flex">
+                    <div className="col d-flex align-items-center justify-content-center">
+                        <i class="fa-solid fa-location-dot fs-2 me-3"></i>
+                        <div>
+                            <p className='mb-0'>Location</p>
+                            <input type="text bg-transparent" placeholder='Your location' />
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-center justify-content-center">
+                        <i class="fa-solid fa-calendar-days fs-2 me-3"></i>
+                        <div>
+                            <p className='mb-0'>Pickup date</p>
+                            <input type="text bg-transparent" placeholder='Enter date' />
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-center border-0 justify-content-center">
+                        <i class="fa-solid fa-calendar-days fs-2 me-3"></i>
+                        <div>
+                            <p className='mb-0'>Return date</p>
+                            <input type="text bg-transparent" placeholder='Enter date' />
+                        </div>
+                    </div>
+                    <div className="col d-flex align-items-center border-0 justify-content-center">
+                        <button className='btn btn-primary'>Search</button>
+                    </div>
+
+
+                </div>
+            </section>
+            <div className="row container gy-4 d-flex d-lg-none position-absolute mobile">
+                <div className="col-12 d-flex align-items-center border-bottom justify-content-center">
+                    <i class="fa-solid fa-location-dot fs-2 me-3"></i>
+                    <div>
+                        <p className='mb-0'>Location</p>
+                        <input type="text bg-transparent" placeholder='Your location' />
+                    </div>
+                </div>
+                <div className="col-6 d-flex align-items-center border-end justify-content-center">
+                    <i class="fa-solid fa-calendar-days fs-2 me-3"></i>
+                    <div>
+                        <p className='mb-0'>Pickup date</p>
+                        <input type="text bg-transparent" placeholder='Enter date' />
+                    </div>
+                </div>
+                <div className="col-6 d-flex align-items-center border-0 justify-content-center">
+                    <i class="fa-solid fa-calendar-days fs-2 me-3"></i>
+                    <div>
+                        <p className='mb-0'>Return date</p>
+                        <input type="text bg-transparent" placeholder='Enter date' />
+                    </div>
+                </div>
+                <div className="col-12 border-top pt-2 mb-4 d-flex align-items-center border-0 justify-content-center">
+                    <button className='btn btn-primary'>Search</button>
+                </div>
+            </div>
+            
+        </>
+      )
+}
