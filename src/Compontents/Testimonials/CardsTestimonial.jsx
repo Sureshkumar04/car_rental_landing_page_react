b7bd0050fca186c9933ad683a1ba1c1e9c0f8ceb
Sureshkumar04 <112899987+Sureshkumar04@users.noreply.github.com>
2024-04-25 20:22:20 +0530

    testio

---
+++ src/Compontents/Testimonials/CardsTestimonial.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import star from '../../assests/Testimonials/star.png'
+
+
+export const CardsTestimonial = ({person, content, name}) => {
+    return (
+        <div>
+            <div className="card testiomal-card mb-3" style={{ maxWidth: '700px', minWidth: '200px' }}>
+                <div className="row g-0">
+                    <div className="col-md-4" style={{maxHeight: '350px', border: 'soild'}}>
+                        <img src={person} className="img-fluid rounded-start" style={{maxHeight: '100%', overflow: 'hidden'}} alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body d-flex flex-column justify-content-between" style={{height: '100%'}}>
+                            <div className='mb-2 mb-sm-0'>
+                                <h1 className="card-title mb-0 fw-medium">5.0 <small className='fs-6'>Stars</small></h1>
+                                <img src={star} className='star-rating'  alt="" />
+                            </div>
+                            <p className="card-text text-body-secondary mb-2 mb-sm-0"><small>“{content}”</small> </p>
+                            <div>
+                                <p className='card-text fw-medium fs-5 mb-0'>{name}</p>
+                                <p className='card-text text-body-secondary fs-6'><small>From Tamil Nadu, India.</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/Compontents/Testimonials/Testimonials.jsx
+++ src/Compontents/Testimonials/Testimonials.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
 import '../Testimonials/Testimonials.css'
+import comma from '../../assests/Testimonials/“.png'
+import commaTwo from '../../assests/Testimonials/“ 2.png'
+import { CardsTestimonial } from './CardsTestimonial'
+import personOne from '../../assests/Testimonials/Person 1.png'
+import personTwo from '../../assests/Testimonials/Person 2.png'
+import personThree from '../../assests/Testimonials/Person 3.png'
+
+
+const testiomalData = [
+    { id: 1, image: personOne, content: "I feel very secure when using caretall's services. Your customer care team is very enthusiastic and the driver is always on time.", name: "Charlie Johnson" },
+    { id: 1, image: personTwo, content: "I feel very secure when using caretall's services. Your customer care team is very enthusiastic and the driver is always on time.", name: "Elizabeth" },
+    { id: 1, image: personThree, content: "I feel very secure when using caretall's services. Your customer care team is very enthusiastic and the driver is always on time.", name: "Charlotte" }
+]
 
 export const Testimonials = () => {
-  return (
-    <div>Testimonials</div>
-  )
+    return (
+        <div className='testimonials mt-5 full-height'>
+            <img src={comma} className='comma' alt="" />
+            <img src={commaTwo} className='comma-2' alt="" />
+            <button className="btn mb-1">Testimonials</button>
+            <h2 className='mx-3 mx-md-0 text-center'>What peole say about us?</h2>
+            <div className='mt-4'>
+
+                {testiomalData.map(data => (
+                    <CardsTestimonial key={data.id}  person={data.image} content={data.content} name={data.name} />
+                ))}
+
+            </div>
+        </div>
+    )
 }
